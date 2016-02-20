0e145938f4c3621786e8d510d373a81640953bd0
Chidozie Ijeomah <chidozie.ijeomah@andela.com>
2016-02-21 00:00:18 +0100

    Changed response test

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-  res.send('hello bendozy');
+  res.send('Welcome to bendozy.com More details coming soon');
 });
 
 app.listen(process.env.PORT || 5000);
