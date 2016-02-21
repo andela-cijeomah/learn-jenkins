baed50a3d40fafdb1f488ae49c3b2b5f748c5bac
Chidozie Ijeomah <chidozie.ijeomah@andela.com>
2016-02-21 01:14:21 +0100

    yello test

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-  res.send('Welcome to bendozy.com More details coming soon');
+  res.send('Yello guest');
 });
 
 app.listen(process.env.PORT || 5000);
--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ var app = require('../app.js');
 
 describe('GET /', function() {
   it('respond with hello world', function(done) {
-    request(app).get('/').expect('Welcome to bendozy.com More details coming soon', done);
+    request(app).get('/').expect('Yello guest', done);
   });
 });
